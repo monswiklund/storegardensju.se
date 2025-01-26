923429a881185a0d73de596e59d6a08f0908e786
Måns <mans@Manss-MacBook-Pro.local>
2025-01-26 22:14:53 +0100

    Update configuration for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/storegardensju-react/', // Detta måste matcha repo-namnet exakt
   plugins: [react()],
+  base: '/',
 })
