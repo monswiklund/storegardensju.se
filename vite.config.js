154a72565183745f68e3b7cba3dbd0e25c877b91
Måns <mans@Manss-MacBook-Pro.local>
2025-01-26 20:37:45 +0100

    Hej

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Sätt base till "/"
+  base: '/storegardensju-react/', // Måste matcha ditt repository-namn
   plugins: [react()],
 })
