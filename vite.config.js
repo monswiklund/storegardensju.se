52601d5c7b9d8038d857678421753b16cdf280b6
Måns <mans@Manss-MacBook-Pro.local>
2025-03-19 15:35:19 +0100

    Fixed base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/storegardensju.se/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
