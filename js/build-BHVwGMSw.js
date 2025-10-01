24b787edd9a55e5a54d2dca3dc761f30284a7afa
monswiklund <monswiklund99@gmail.com>
2025-10-01 13:30:43 +0200

    Updates

---
+++ js/build-BHVwGMSw.js
@@ -0,0 +1,1 @@
+const u="2.0.0",b="2025-10-01T11:30:40.597Z",e=1759318240598,i={version:u,buildTime:b,buildNumber:1759318240598};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-Dk-97YxV.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.0",b="2025-10-01T11:22:46.146Z",e=1759317766146,i={version:u,buildTime:b,buildNumber:1759317766146};export{e as buildNumber,b as buildTime,i as default,u as version};
