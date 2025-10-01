767100a02ad2a44c3bbc527c49b18103348a317c
monswiklund <monswiklund99@gmail.com>
2025-10-01 13:55:06 +0200

    Updates

--- js/build-BaA42-zu.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.0",b="2025-10-01T11:54:24.110Z",e=1759319664112,i={version:u,buildTime:b,buildNumber:1759319664112};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-BlQAZ0WB.js
@@ -0,0 +1,1 @@
+const u="2.0.0",b="2025-10-01T11:55:03.644Z",e=1759319703645,i={version:u,buildTime:b,buildNumber:1759319703645};export{e as buildNumber,b as buildTime,i as default,u as version};
