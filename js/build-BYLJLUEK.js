e504362d584dd15bce981aa80bda31811025bd0a
monswiklund <monswiklund99@gmail.com>
2025-10-02 09:31:45 +0200

    Updates

--- js/build-BYLJLUEK.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.0",b="2025-10-02T07:28:29.601Z",e=1759390109601,i={version:u,buildTime:b,buildNumber:1759390109601};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-DaxkPv8J.js
@@ -0,0 +1,1 @@
+const u="2.0.0",b="2025-10-02T07:31:42.782Z",e=1759390302782,i={version:u,buildTime:b,buildNumber:1759390302782};export{e as buildNumber,b as buildTime,i as default,u as version};
