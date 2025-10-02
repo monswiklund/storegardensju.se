c2338ec2652e7198f74ebdda138345eb9e802916
monswiklund <monswiklund99@gmail.com>
2025-10-02 13:05:14 +0200

    Updates

--- js/build-B9XoLEle.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.0",b="2025-10-02T11:04:58.836Z",e=1759403098837,i={version:u,buildTime:b,buildNumber:1759403098837};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-BSqy5V9V.js
@@ -0,0 +1,1 @@
+const u="2.0.0",b="2025-10-02T11:05:11.902Z",e=1759403111903,i={version:u,buildTime:b,buildNumber:1759403111903};export{e as buildNumber,b as buildTime,i as default,u as version};
