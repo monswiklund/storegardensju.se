8127cf5664a54a90c16bf2a4fd59f0cbd1e2476a
monswiklund <monswiklund99@gmail.com>
2025-10-07 17:48:41 +0200

    Updates

--- js/build-DjZWo-EN.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-10-06T07:15:16.987Z",e=1759734916988,i={version:u,buildTime:b,buildNumber:1759734916988};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-pKGyEGck.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-10-07T15:48:37.392Z",e=1759852117393,i={version:u,buildTime:b,buildNumber:1759852117393};export{e as buildNumber,b as buildTime,i as default,u as version};
