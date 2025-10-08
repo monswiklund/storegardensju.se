c50d5a642315098acd0e5a318f416ae1cff14cf2
monswiklund <monswiklund99@gmail.com>
2025-10-08 12:18:46 +0200

    Updates

--- js/build-7WABM8pL.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-10-08T10:11:47.077Z",e=1759918307078,i={version:u,buildTime:b,buildNumber:1759918307078};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-CVQC3xRJ.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-10-08T10:18:43.172Z",e=1759918723173,i={version:u,buildTime:b,buildNumber:1759918723173};export{e as buildNumber,b as buildTime,i as default,u as version};
