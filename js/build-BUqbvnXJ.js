b6e4774435b6dac365d24274460429fa4689db5d
monswiklund <monswiklund99@gmail.com>
2025-10-08 13:09:14 +0200

    Updates

--- js/build-BUqbvnXJ.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-10-08T11:05:19.735Z",e=1759921519735,i={version:u,buildTime:b,buildNumber:1759921519735};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-Df-st6zO.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-10-08T11:09:11.731Z",e=1759921751731,i={version:u,buildTime:b,buildNumber:1759921751731};export{e as buildNumber,b as buildTime,i as default,u as version};
