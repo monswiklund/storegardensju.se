0baaf89dd02c8effc4142b3fda3ff096620dfc0b
monswiklund <monswiklund99@gmail.com>
2025-10-20 19:27:22 +0200

    Update EventPage styles and content; enhance layout, add hero description section, and adjust build configuration

--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -43,14 +43,26 @@ Mingel 300+`}
         zIndex={2}
       />
 
-     {/* Sticky Image Section 2 */}
-        <ParallaxHero
-            image="/images/event/hero/hero-3.webp"
-            title="Vi har allt som behövs för ett lyckat event"
-            subtitle="Läs mer nedan vad vi erbjuder"
-            zIndex={3}
-        />
-        <SendToGalleryButton />
+      {/* Sticky Image Section 2 */}
+      <ParallaxHero
+        image="/images/event/hero/hero-3.webp"
+        title="Vi har allt som behövs för ett lyckat event"
+        subtitle="Läs mer nedan vad vi erbjuder"
+        zIndex={3}
+      />
+      <section className="event-hero-description">
+        <div className="event-hero-description__content">
+          <p>Vare sig om det är ett bröllop, 50 års fest eller en afterwork så passar våran gård er!</p>
+          <p>Våran lokal består av ca 360 kvadratmeter inomhus fördelat på två våningar, lada och loft.</p>
+          <p>På loftet kan man sitta över 150 gäster om så önskas, men det passar även fint till de mindre sällskapen.</p>
+          <p>Den nedre delen av ladan är våran umgängesyta. Där finns långbord, soffor, dansgolv, bar, kök samt ett förråd. Toaletter finns intill.</p>
+          <p>Hos oss hittar ni allt ni kan tänkas behöva för ett event. Mat och dryck tar ni med själva.</p>
+          <p>Vi har utrustning som glas, tallrikar, bestick, bord, stolar, ljud och ljus mm.</p>
+          <p>Vi kan även erbjuda erfaren personal som arbetar med servering, bar eller dj.</p>
+          <p>Kontakta oss så hjälper vi gärna till om ni har några funderingar.</p>
+        </div>
+      </section>
+      <SendToGalleryButton />
     </main>
   );
 }
