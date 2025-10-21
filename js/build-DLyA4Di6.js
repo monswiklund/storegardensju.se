44e6d1235a4ef1bd157958e5c14d809c0c7d44c0
monswiklund <monswiklund99@gmail.com>
2025-10-21 10:16:03 +0200

    Updates

--- js/build-DLyA4Di6.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-10-21T07:56:26.485Z",e=1761033386486,i={version:u,buildTime:b,buildNumber:1761033386486};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-DeU2Ne_4.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-10-21T08:15:59.741Z",e=1761034559742,i={version:u,buildTime:b,buildNumber:1761034559742};export{e as buildNumber,b as buildTime,i as default,u as version};
