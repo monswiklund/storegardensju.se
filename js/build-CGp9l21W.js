c1d6d095490acd03ffdf8a8bf3d13d72742f170c
monswiklund <monswiklund99@gmail.com>
2025-11-10 12:14:45 +0100

    Updates

--- js/build-CGp9l21W.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-11-05T13:07:30.619Z",e=1762348050620,i={version:u,buildTime:b,buildNumber:1762348050620};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-oVA2KM68.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-11-10T11:14:41.875Z",e=1762773281876,i={version:u,buildTime:b,buildNumber:1762773281876};export{e as buildNumber,b as buildTime,i as default,u as version};
