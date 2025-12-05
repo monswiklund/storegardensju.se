0865c1faa7df075a05f93b34051843f7141dd2ac
monswiklund <monswiklund99@gmail.com>
2025-12-05 21:01:05 +0100

    Updates

---
+++ js/build-CPa6l4FX.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-12-05T20:01:01.594Z",e=1764964861595,i={version:u,buildTime:b,buildNumber:1764964861595};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-D7z_LQ-s.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-12-05T19:15:25.353Z",e=1764962125354,i={version:u,buildTime:b,buildNumber:1764962125354};export{e as buildNumber,b as buildTime,i as default,u as version};
