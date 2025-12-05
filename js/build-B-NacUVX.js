acfac2d16edcfb103838d062c9160e4da4a8be66
monswiklund <monswiklund99@gmail.com>
2025-12-05 21:12:33 +0100

    Updates

--- js/build-B-NacUVX.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-12-05T20:09:11.684Z",e=1764965351685,i={version:u,buildTime:b,buildNumber:1764965351685};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-CLy1uzEK.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-12-05T20:12:29.968Z",e=1764965549969,i={version:u,buildTime:b,buildNumber:1764965549969};export{e as buildNumber,b as buildTime,i as default,u as version};
