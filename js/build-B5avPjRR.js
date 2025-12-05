487b802a720ac7f99cd884e363f4f8c2271fda30
monswiklund <monswiklund99@gmail.com>
2025-12-05 18:06:21 +0100

    Updates

---
+++ js/build-B5avPjRR.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-12-05T17:06:17.970Z",e=1764954377971,i={version:u,buildTime:b,buildNumber:1764954377971};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-EZGsy5vi.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-11-26T08:36:17.720Z",e=1764146177720,i={version:u,buildTime:b,buildNumber:1764146177720};export{e as buildNumber,b as buildTime,i as default,u as version};
