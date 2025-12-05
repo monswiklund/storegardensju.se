60910cd1cf3b90679a30bb01e1556eed078a861b
monswiklund <monswiklund99@gmail.com>
2025-12-05 19:47:41 +0100

    Updates

---
+++ js/build-DgMeC2U9.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-12-05T18:47:38.021Z",e=1764960458022,i={version:u,buildTime:b,buildNumber:1764960458022};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-fqr04B6l.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-12-05T18:38:20.467Z",e=1764959900468,i={version:u,buildTime:b,buildNumber:1764959900468};export{e as buildNumber,b as buildTime,i as default,u as version};
