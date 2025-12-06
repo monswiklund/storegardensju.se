02f7411e4be8d758aff58bd4a4f4e9c50402645c
monswiklund <monswiklund99@gmail.com>
2025-12-06 10:02:48 +0100

    Updates

--- js/build-B8F_zePD.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-12-06T08:55:00.907Z",e=1765011300908,i={version:u,buildTime:b,buildNumber:1765011300908};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-DWP688AX.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-12-06T09:02:45.236Z",e=1765011765238,i={version:u,buildTime:b,buildNumber:1765011765238};export{e as buildNumber,b as buildTime,i as default,u as version};
