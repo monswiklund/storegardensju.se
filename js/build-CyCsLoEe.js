bf66cf46d107c553b8f6f2aed28d27cfcbad5dff
monswiklund <monswiklund99@gmail.com>
2025-12-15 15:54:56 +0100

    Updates

---
+++ js/build-CyCsLoEe.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2025-12-15T14:54:52.712Z",e=1765810492713,i={version:u,buildTime:b,buildNumber:1765810492713};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-DUlvSxk-.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2025-12-15T14:25:04.245Z",e=1765808704246,i={version:u,buildTime:b,buildNumber:1765808704246};export{e as buildNumber,b as buildTime,i as default,u as version};
