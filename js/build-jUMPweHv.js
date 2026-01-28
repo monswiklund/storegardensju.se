90f24209d4b5860cda8e271183c0ed37d5b481fb
monswiklund <monswiklund99@gmail.com>
2026-01-28 19:26:27 +0100

    Updates

---
+++ js/build-jUMPweHv.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-01-28T18:26:23.329Z",e=1769624783330,i={version:u,buildTime:b,buildNumber:1769624783330};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-uwPPhkDw.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-01-28T18:09:18.563Z",e=1769623758564,i={version:u,buildTime:b,buildNumber:1769623758564};export{e as buildNumber,b as buildTime,i as default,u as version};
