f31d01bc1bb138eb5e16ab37db348cf270aeeceb
monswiklund <monswiklund99@gmail.com>
2026-01-28 18:54:32 +0100

    Updates

---
+++ js/build-B0_TuAwe.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-01-28T17:54:28.268Z",e=1769622868268,i={version:u,buildTime:b,buildNumber:1769622868268};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-CznnUJhi.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-01-28T17:48:03.784Z",e=1769622483785,i={version:u,buildTime:b,buildNumber:1769622483785};export{e as buildNumber,b as buildTime,i as default,u as version};
