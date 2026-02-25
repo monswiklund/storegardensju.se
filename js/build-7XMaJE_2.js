78052094bbc51ea74ffad86f77bd7431e632e32e
monswiklund <monswiklund99@gmail.com>
2026-02-25 19:04:19 +0100

    Updates

--- js/build-7XMaJE_2.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-02-25T17:58:26.910Z",e=1772042306911,i={version:u,buildTime:b,buildNumber:1772042306911};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-Bvr7kF8d.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-02-25T18:04:14.497Z",e=1772042654498,i={version:u,buildTime:b,buildNumber:1772042654498};export{e as buildNumber,b as buildTime,i as default,u as version};
