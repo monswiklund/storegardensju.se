a82fa7f63f0ed621181fde853063f8745959cc67
monswiklund <monswiklund99@gmail.com>
2026-02-25 20:41:59 +0100

    Updates

--- js/build-CIzwC8yt.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-02-25T18:58:24.645Z",e=1772045904646,i={version:u,buildTime:b,buildNumber:1772045904646};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-CLcdJYhI.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-02-25T19:41:53.333Z",e=1772048513334,i={version:u,buildTime:b,buildNumber:1772048513334};export{e as buildNumber,b as buildTime,i as default,u as version};
