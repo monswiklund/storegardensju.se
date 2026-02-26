dd5a4bd53a67929c09ac99c2a6a60d87c64bde99
monswiklund <monswiklund99@gmail.com>
2026-02-26 18:38:02 +0100

    Updates

--- js/build-BW-H2I28.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-02-26T16:48:09.348Z",e=1772124489350,i={version:u,buildTime:b,buildNumber:1772124489350};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-BqOADhB7.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-02-26T17:37:58.911Z",e=1772127478912,i={version:u,buildTime:b,buildNumber:1772127478912};export{e as buildNumber,b as buildTime,i as default,u as version};
