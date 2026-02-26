a8a7ad526f9e3b525a8c95a793cc4b6c13d32a6b
monswiklund <monswiklund99@gmail.com>
2026-02-26 20:05:13 +0100

    Updates

--- js/build-CrInKNUR.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-02-26T19:02:38.359Z",e=1772132558360,i={version:u,buildTime:b,buildNumber:1772132558360};export{e as buildNumber,b as buildTime,i as default,u as version};
---
+++ js/build-I9_NZXyO.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-02-26T19:05:09.888Z",e=1772132709889,i={version:u,buildTime:b,buildNumber:1772132709889};export{e as buildNumber,b as buildTime,i as default,u as version};
