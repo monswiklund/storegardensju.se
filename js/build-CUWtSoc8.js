29302ff44367a1f60ce54aa244235525e9fa6690
monswiklund <monswiklund99@gmail.com>
2026-03-07 10:32:01 +0100

    Updates

---
+++ js/build-CUWtSoc8.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-03-07T09:31:57.561Z",e=1772875917562,i={version:u,buildTime:b,buildNumber:1772875917562};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-DuyHzkWB.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-03-07T09:23:53.941Z",e=1772875433942,i={version:u,buildTime:b,buildNumber:1772875433942};export{e as buildNumber,b as buildTime,i as default,u as version};
