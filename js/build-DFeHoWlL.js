e795e0ee4c276bd5990d09f0445055f23016535a
monswiklund <monswiklund99@gmail.com>
2026-07-04 23:19:04 +0200

    Updates

---
+++ js/build-DFeHoWlL.js
@@ -0,0 +1,1 @@
+const u="2.0.1",b="2026-07-04T21:18:59.408Z",e=1783199939409,i={version:u,buildTime:b,buildNumber:1783199939409};export{e as buildNumber,b as buildTime,i as default,u as version};
--- js/build-DXglXRVr.js
+++
@@ -1,1 +0,0 @@
-const u="2.0.1",b="2026-07-04T19:10:04.392Z",e=1783192204393,i={version:u,buildTime:b,buildNumber:1783192204393};export{e as buildNumber,b as buildTime,i as default,u as version};
