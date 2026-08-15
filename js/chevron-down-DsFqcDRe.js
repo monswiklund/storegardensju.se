cc7c519248d4422f36a1ce434d201cc96728fb3b
monswiklund <monswiklund@users.noreply.github.com>
2026-08-15 09:56:53 +0000

    deploy: faa5a41a5b1a94ecedfff44fac88706d2715e17f

---
+++ js/chevron-down-DsFqcDRe.js
@@ -0,0 +1,6 @@
+import{c as o}from"./index-BL3n9Px2.js";/**
+ * @license lucide-react v0.454.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=o("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);export{n as C};
